112f172d9f86c3755157f0c4d613282c8f371ee9
s1nfyy <blogerslu@gmail.com>
2024-05-24 01:05:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
     lines: [
         "Te amo puta",
-        "花开一季，有落红为证",
-        "逝去的岁月，如何能托于烟尘",
+        "Sos el amor de todas mis vidas",
+        "Con todo mi amor te amo",
         "你的浅笑，是我永生眷恋",
         "相思的真情，是否能明白一二",
         "亲爱的宝贝，你是我的唯一",
