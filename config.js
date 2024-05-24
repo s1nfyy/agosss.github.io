ef999b7f4a472a1acd6609376d8ed46dd357c9ce
s1nfyy <blogerslu@gmail.com>
2024-05-24 01:45:17 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 var config = {
     lines: [
-        "Te amo puta",
-        "Sos el amor de todas mis vidas",
-        "Con todo mi amor te amo",
-        "你的浅笑，是我永生眷恋",
-        "相思的真情，是否能明白一二",
-        "亲爱的宝贝，你是我的唯一",
-        "爱你永远。。。",
+        "Cuando te conocí, nunca imaginé que llegarías a ser tan especial para mí. Con el tiempo, nuestra amistad se llenó de cariño y risas, y sin darme cuenta, me fui enamorando de ti. Aunque nunca te lo confesé por miedo al rechazo, me sorprendió descubrir que también te habías enamorado de mí, y di el primer paso para conquistarnos.",
+        "Hoy, después de tanto tiempo juntos, puedo decir que cada momento contigo es inolvidable. Me haces sentir muy especial y muy feliz, y agradezco profundamente tu amor y dedicación. Prometo luchar por nuestro amor y construir un futuro juntos. Te amaré siempre y hasta el infinito y más allá. ¡Te amo puta!                ❤️",
     ],
 }
